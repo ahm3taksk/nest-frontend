2da704ecc1290ac7f22a487bd3fb628092c376ae
Ahmet Dursun <ahmetdursun4587@protonmail.com>
2024-02-14 23:44:00 +0300

    Added popup menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,14 @@ tailwind.config = {
       }
     }
   }
+
+function activateSidePopup() {
+    var popup = document.getElementsByClassName("sidePopupContent")[0];
+    if (popup.classList.contains("active")) {
+        popup.classList.remove("active");
+        popup.classList.remove("row");
+    } else {
+        popup.classList.add("active");
+        popup.classList.add("row");
+    }
+}
